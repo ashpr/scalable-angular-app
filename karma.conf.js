d5e16e44b56d7af08e50e0bc45aa3c9b1969231c
ashpr <ashprosser@live.co.uk>
2020-06-23 15:04:58 +0100

    fix: turn off autowatch for CI

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function (config) {
       port: 9876,
       colors: true,
       logLevel: config.LOG_INFO,
-      autoWatch: true,
+      autoWatch: environment.CI !== 'true',
       browsers: ['ChromeHeadless'],
       singleRun: false
     });
